969149d2ba6b2563b6817c790faaf2af2cb142ef
Gavyn Elrick <gavynelrick@gmail.com>
2025-01-23 15:58:22 -0500

    Moving Quiz App demo from local env to github for public source code display.
    Do to the short scope of this practice project. It has only taken a couple hours and didn't require prior commits.

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+function QuestionTimer({ timeout, onTimeout, mode }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    console.log("Setting timeout");
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => clearTimeout(timer);
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    console.log("Setting interval");
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 100);
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    />
+  );
+}
+
+export default QuestionTimer;
